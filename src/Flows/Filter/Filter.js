ae1b90e5ca91cc74d3ea36227943b9713e3022a7
AndrewLolo <andrewloloal@gmail.com>
2016-03-13 22:06:06 +0200

    npmignore and minor flows fix

--- src/Flows/Filter/Filter.js
+++ src/Flows/Filter/Filter.js
@@ -1,7 +1,7 @@
 export default (array, handler) => {
     let resultArray = [];
     for (let i = 0; i < array.length; ++i) {
-        if (handler(array[i])) {
+        if (handler(array[i], i, array)) {
             resultArray.push(array[i]);
         }
     }
